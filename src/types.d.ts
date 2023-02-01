5c72e6e435a3f7309c020852a39cc10fcdf0756e
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2023-02-01 21:00:33 +0700

    set up project add packages add types setup webpack

---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+// declare module "*.scss" {
+//   const content: Record<string, string>;
+//   export default content;
+// }
+
+declare module "*.svg" {
+  const src: any;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: any;
+  export default src;
+}
+
+declare module "*.ts";
